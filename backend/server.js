a260e0ec8d8d44441f41bc32b8090a4fe519652d
Hanielly Silva Marques <hanielly.marques@iesp.edu.br>
2026-02-13 16:27:46 -0300

    configura CORS para frontend

--- backend/server.js
+++ backend/server.js
@@ -4,29 +4,24 @@ import chatRoutes from "./routes/chat.routes.js";
 import suggestionsRoutes from "./routes/suggestions.js";
 import pool from "./config/database.js";
 
-
-
-
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "https://chatbot-biblioteca-frontend.onrender.com",
+  methods: ["GET", "POST"],
+  allowedHeaders: ["Content-Type"],
+}));
 
 app.use(express.json());
 
 app.use("/chat", chatRoutes);
-
 app.use("/suggestions", suggestionsRoutes);
 
 pool.query("SELECT NOW()")
   .then(res => console.log("Banco conectado:", res.rows))
   .catch(err => console.error("Erro ao conectar:", err));
 
-
 const PORT = process.env.PORT || 3000;
-
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
-
-
-
