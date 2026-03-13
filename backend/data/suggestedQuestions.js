7f437f727c96930066e96e4cb29ee3a7df9cd38a
Hanielly Silva Marques <hanielly.marques@iesp.edu.br>
2026-03-13 10:23:52 -0300

    botoes de sugestões adicionados

--- backend/data/suggestedQuestions.js
+++ backend/data/suggestedQuestions.js
@@ -2,7 +2,9 @@ const suggestedQuestions = [
     "O que o IndexIA pode fazer?",
     "Como faço para pegar livro?",
     "Qual é o horário da biblioteca?",
-    "Preciso de ajuda. Como entro em contato?"
+    "Preciso de ajuda. Como entro em contato?",
+    "Quero participar do clube do livro",
+    "Meu livro atrasou. E agora?"
 
 ];
 
